f02802265a7aade17edf63798b58bef76d7e1873
Prakhar Pandey <prakhar10may@gmail.com>
2024-04-12 23:36:07 +0530

    models & server

--- scraper/src/models/product.model.js
+++ scraper/src/models/product.model.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema(
-  {
-    cardURL: { type: String },
-    company: { type: String },
-    category: { type: String },
-    productName: { type: String },
-    sponsored: { type: String },
-    badge: { type: String },
-    price: { type: String },
-    basePrice: { type: String },
-    rating: { type: String },
-    ratingsNumber: { type: String },
-    boughtPastMonth: { type: String },
-    reviews: [{ type: String }],
-  },
-  { timestamps: true }
-);
+  const ProductSchema = new mongoose.Schema(
+    {
+      cardURL: { type: String },
+      company: { type: String },
+      category: { type: String },
+      productName: { type: String },
+      sponsored: { type: String },
+      badge: { type: String },
+      price: { type: String },
+      basePrice: { type: String },
+      rating: { type: String },
+      ratingsNumber: { type: String },
+      boughtPastMonth: { type: String },
+      reviews: [{ type: String }],
+    },
+    { timestamps: true }
+  );
 
-const Product = mongoose.model("Product", ProductSchema);
+  const Product = mongoose.model("Product", ProductSchema);
 
-module.exports = { Product };
+  module.exports = { Product };
